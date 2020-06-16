b86ba941f152325669862f0a6557eb4606237da0
alpii <32803988+alpii@users.noreply.github.com>
2020-06-16 13:13:12 +0300

    Update asistan.py

--- asistan.py
+++ asistan.py
@@ -6,7 +6,7 @@ import time
 import datetime
 import os
 import locale
-import win32api
+import win32api # .exeye çevirmede sıkıntı çıkartıyor.
 from win32con import VK_MEDIA_PLAY_PAUSE, KEYEVENTF_EXTENDEDKEY, VK_VOLUME_MUTE, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK
 
 # win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0)
@@ -33,15 +33,16 @@ def get_audio():
             if said == "İleri" or said == "İlerle":
                 said = "ileri"
         except Exception as e: # Mikrofonun algılanmaması, google'a bağlanamama gibi durumlarda
-            return ""
+            pass
     return said.lower()
 
 
 def checkcommand(list, audio, event, speak1): # Bu fonksiyonu söylenilenler içinde belli bir liste kelime aratmak için kullandım
     for i in list:
         if audio.count(i) >= 1:
-            speak(speak1)
+            print(speak1)
             win32api.keybd_event(event, 0, KEYEVENTF_EXTENDEDKEY, 0)
+            speak(speak1)
             print("Şu işlemi yaptım: " + list)
             time.sleep(3)
 
@@ -51,8 +52,8 @@ def checksleep(list, audio, sleep1, speak1, speak2): # Yukarıdaki fonksiyonla a
     for i in list:
         if audio.count(i) >= 1:
             print("Uyuyorum.")
-            speak(speak1)
             time.sleep(sleep1)
+            speak(speak1)
             print("Uyandım.")
             speak(speak2)
 
@@ -61,46 +62,42 @@ locale.setlocale(locale.LC_ALL, 'turkish')
 zaman = datetime.datetime.now()
 tarih = datetime.datetime.strftime(zaman, '%c')
 
-
-
-
-speak("Asistan Devrede")
 print(tarih + " Çalıştırıldı...")
-
+speak("Asistan Devrede")
 
 #kelime listeleri
 
-KEYWORD = "asistan"
+KEYWORD = "asistan" 
 PLAYPAUSE = ["dur", "başlat", "devam", "durdur", "devam ettir"]
 FORWARD = ["İleri", "ileri", "İlerle", "ileri", "illeri", "geç", "next", "skip", "sonraki", "değiş", "diğeri", "atla", "Müziği değiştir", "müziği değiştir"]
 BACKWARD = ["Geri", "geri", "önceki", "az önce", "önceki müzik"]
 MUTE = ["Sustur", "mute", "sesi kapat", "sesi indir", "sus", "müziği sustur", "sesleri kes", "sesleri aç", "unmute", "susturmayı aç", "konuş", "aç"]
 SLEEP = ["dinlen", "mola", "kısa kes", "yeter"]
-LONGSLEEP = ["kafana göre takıl", "takıl kafana göre", "teşekkür ederim", "sağol canım", "long sleep", "afk", "uyu"]
-QUIT = ["Çık", "çık", "çıkış", "kapat", "programı sonlandır", "sonlandır", "quit", "exit", "kendine iyi bak"]
+LONGSLEEP = ["kafana göre takıl", "takıl kafana göre", "teşekkür ederim", "sağol canım", "afk", "uyu"]
+QUIT = ["Çık", "çık", "çıkış", "kapat", "programı sonlandır", "sonlandır", "quit", "exit", "kendine iyi bak", "hadi eyvallah"]
 
 # main loop
+if __name__ == '__main__':
+    while True:
+        kwdedect = "\n"
+        kwdedect = get_audio()
 
-while True:
-    kwdedect = "\n"
-    kwdedect = get_audio()
+        try:
+            if kwdedect.count(KEYWORD) >= 1:
+                checkcommand(PLAYPAUSE, kwdedect, VK_MEDIA_PLAY_PAUSE, "Başlatıyorum ya da Durduruyorum")
+                checkcommand(FORWARD, kwdedect, VK_MEDIA_NEXT_TRACK, "Bir sonraki şarkıya geçiyorum")
+                checkcommand(BACKWARD, kwdedect, VK_MEDIA_PREV_TRACK, "Bir önceki şarkıya geçiyorum")
+                checkcommand(MUTE, kwdedect, VK_VOLUME_MUTE, "Sistem seslerini susturuyorum")
+                checksleep(SLEEP, kwdedect, 35, "Kısa bir mola veriyorum", "Günaydın")
+                checksleep(LONGSLEEP, kwdedect, 120, "Uzun bir mola veriyorum", "Günaydın")
 
-    try:
-        if kwdedect.count(KEYWORD) >= 1:
-            checkcommand(PLAYPAUSE, kwdedect, VK_MEDIA_PLAY_PAUSE, "Başlatıyorum ya da Durduruyorum")
-            checkcommand(FORWARD, kwdedect, VK_MEDIA_NEXT_TRACK, "Bir sonraki şarkıya geçiyorum")
-            checkcommand(BACKWARD, kwdedect, VK_MEDIA_PREV_TRACK, "Bir önceki şarkıya geçiyorum")
-            checkcommand(MUTE, kwdedect, VK_VOLUME_MUTE, "Sistem seslerini susturuyorum")
-            checksleep(SLEEP, kwdedect, 35, "Kısa bir mola veriyorum", "Günaydın")
-            checksleep(LONGSLEEP, kwdedect, 120, "Uzun bir mola veriyorum", "Günaydın")
-
-            for i in QUIT:
-                if kwdedect.count(i) >= 1:
-                    speak("İyi günler")
-                    print(tarih + " Çıkış yapılıyor...")
-                    quit()
+                for i in QUIT:
+                    if kwdedect.count(i) >= 1:
+                        print(tarih + " Çıkış yapılıyor...")
+                        speak("İyi günler")
+                        quit()
 
 
-            time.sleep(1)
-    except TypeError:
-        print("Algılanamadı.")
+                time.sleep(1)
+        except TypeError:
+            print("Algılanamadı.")
